31b5e26f823d4f603844a8344e69c1c4203c9cc7
Alanis Hadama <102327029+hadamas@users.noreply.github.com>
2024-02-29 15:31:55 -0300

    Cefet update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -14,5 +14,16 @@
 
 int	main(int ac, char *av[])
 {
-	
+    t_stack *a;
+    t_stack *b;
+    
+    a = NULL;
+    b = NULL;
+    
+    if (ac != 1 || ac == 2 && !av[1][0])
+        return (1);
+    else if (ac == 2)
+        av = split(av[1], 32);
+        
+    
 }
